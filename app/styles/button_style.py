f3c774aa42123bda7a40d0c20f7c188c0558f1c1
gyuhyeong <kgh0730@gmail.com>
2025-03-10 20:59:04 +0900

    Feat: Button style 구현

---
+++ app/styles/button_style.py
@@ -0,0 +1,24 @@
+"""button style"""
+
+import streamlit as st
+
+
+def set_button_style():
+    """set streamlit button style"""
+
+    style = """
+    <style>
+        .st-emotion-cache-f6mfty {
+        }
+        .st-emotion-cache-f6mfty:hover {
+            box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+            border: 1px solid rgba(49, 51, 63, 0.2) !important;
+        }
+        .st-emotion-cache-f6mfty:active {
+            box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset;
+            background-color: rgb(244, 241, 235);
+            
+        }
+    </style>
+    """
+    st.markdown(body=style, unsafe_allow_html=True)
