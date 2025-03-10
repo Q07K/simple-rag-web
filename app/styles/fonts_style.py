74f33ddd79cce2d4afb3f2dfe2cb1cbeb2cca1e4
gyuhyeong <kgh0730@gmail.com>
2025-03-10 20:58:50 +0900

    Feat: App Fonts style 구현

---
+++ app/styles/fonts_style.py
@@ -0,0 +1,23 @@
+"""fonts style"""
+
+import streamlit as st
+
+
+def set_font():
+    """set streamlit fonts style"""
+
+    style = """
+    <style>
+        @font-face {
+            font-family: 'Pretendard-Regular';
+            src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+            font-weight: 400;
+            font-style: normal;
+        }
+        html, body, div, p, h1, h2, h3, [class*="css"] {
+            font-family: 'Pretendard-Regular', sans-serif !important;
+            color: #212121;
+        }
+    </style>
+    """
+    st.markdown(body=style, unsafe_allow_html=True)
