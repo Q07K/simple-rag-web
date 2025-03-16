f8a7f1793df5dcd83d383118875e0511dd3f0b00
gyuhyeong <kgh0730@gmail.com>
2025-03-17 01:18:58 +0900

    Feat: Chat API handler 구현

---
+++ apis/chats_api.py
@@ -0,0 +1,22 @@
+from apis import api_url
+from apis.api_wrapper import ApiWrapper
+from schemas.chat_response import ChatsResponse
+from schemas.message_response import MessagesModel, MessagesResponse
+
+
+def get_chats() -> ChatsResponse | None:
+    return ApiWrapper().get(url=api_url.CHATS, schema=ChatsResponse)
+
+
+def get_messages_by_chat_id(chat_id: str) -> MessagesModel | None:
+    return ApiWrapper().get(
+        url=api_url.CHATS + f"/{chat_id}",
+        schema=MessagesResponse,
+    )
+
+
+def delete_chat_by_chat_id(chat_id: str) -> ChatsResponse | None:
+    return ApiWrapper().delete(
+        url=api_url.CHATS + f"/{chat_id}",
+        schema=ChatsResponse,
+    )
