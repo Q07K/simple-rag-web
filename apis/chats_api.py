3fdbf1927c0a28cfa361121b0b98ab4976223223
gyuhyeong <kgh0730@gmail.com>
2025-03-17 02:09:10 +0900

    Fix: Return type 표기 오류

--- apis/chats_api.py
+++ apis/chats_api.py
@@ -1,14 +1,14 @@
 from apis import api_url
 from apis.api_wrapper import ApiWrapper
 from schemas.chat_response import ChatsResponse
-from schemas.message_response import MessagesModel, MessagesResponse
+from schemas.message_response import MessagesResponse
 
 
 def get_chats() -> ChatsResponse | None:
     return ApiWrapper().get(url=api_url.CHATS, schema=ChatsResponse)
 
 
-def get_messages_by_chat_id(chat_id: str) -> MessagesModel | None:
+def get_messages_by_chat_id(chat_id: str) -> MessagesResponse | None:
     return ApiWrapper().get(
         url=api_url.CHATS + f"/{chat_id}",
         schema=MessagesResponse,
