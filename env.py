85777061661704bee654552e26702513033bc0b4
gyuhyeong <kgh0730@gmail.com>
2025-03-16 23:15:46 +0900

    Feat: Environment variables module

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# API
+API_HOST = os.getenv(key="API_HOST", default="localhost")
+API_PORT = os.getenv(key="API_PORT", default="8000")
+API_URL = f"{API_HOST}:{API_PORT}"
+API_VERSION = os.getenv(key="API_VERSION", default="")
