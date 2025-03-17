29f76118d921e7d5af14531921ac77058c576cad
gyuhyeong <kgh0730@gmail.com>
2025-03-18 03:54:18 +0900

    Feat: Chats API Request

--- schemas/chat_request.py
+++ schemas/chat_request.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 
+from schemas.message_response import MessagesModel
+
 
 class ChatInitiateRequest(BaseModel):
     temperature: float
     max_tokens: int
     query: str
+
+
+class ChatRequest(ChatInitiateRequest, MessagesModel): ...
