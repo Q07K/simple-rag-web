2a05d588da1de41c1f5e83a57315fc09251eb8f5
gyuhyeong <kgh0730@gmail.com>
2025-03-18 03:53:35 +0900

    Feat: 모델 설정 추가

--- widgets/side_bar_widget.py
+++ widgets/side_bar_widget.py
@@ -1,13 +1,36 @@
 import streamlit as st
 
 from apis import chats_api
+from services.chats_service import delete_session_state_messages
 from widgets.chats.chat_session_widget import chat_session_block
 
 
 def sidebar():
     with st.sidebar:
+        st.header("모델 설정")
+        st.slider(
+            label="temperature",
+            key="temperature",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+        )
+        st.slider(
+            label="max_tokens",
+            key="max_tokens",
+            min_value=0,
+            max_value=1000,
+            value=100,
+        )
+        st.header(" 대화")
+        st.button(
+            "새로운 대화",
+            use_container_width=True,
+            icon="➕",
+            on_click=delete_session_state_messages,
+        )
         response = chats_api.get_chats()
         if response:
-            models = response.data.chats
+            models = response.data.chats[::-1]
             for model in models:
                 chat_session_block(model=model)
