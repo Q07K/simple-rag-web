a439ee8daa18a4998814dfd4b0755408c6579b25
gyuhyeong <kgh0730@gmail.com>
2025-03-18 03:55:06 +0900

    Feat: dialog 수정 및 delete session 추가

--- services/chats_service.py
+++ services/chats_service.py
@@ -10,18 +10,25 @@ def get_messages(chat_id: str):
     if response:
         models = response.data.messages
         st.session_state.messages = models
+        st.session_state.chat_id = chat_id
 
 
 @st.dialog("Delete Chat")
 def delete_chat(model: ChatModel):
-    st.markdown(f"""### Title:\n\n`{model.name}`을 정말 삭제하시겠습니까?""")
+    st.markdown(f"""### Title:`{model.name}`\n\n정말 삭제하시겠습니까?""")
+    process_field = st.container()
     _, left_field, _, right_field, _ = st.columns([0.1, 0.3, 0.1, 0.3, 0.1])
     delete_no = left_field.button("No", use_container_width=True)
     delete_yes = right_field.button("Yes", use_container_width=True)
     if delete_yes:
-        with st.spinner("처리중..."):
-            chats_api.delete_chat_by_chat_id(chat_id=model.id)
+        with process_field:
+            with st.spinner("처리중..."):
+                chats_api.delete_chat_by_chat_id(chat_id=model.id)
         st.rerun()
 
     if delete_no:
         st.rerun()
+
+
+def delete_session_state_messages():
+    st.session_state.messages = []
